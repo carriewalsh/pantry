aac973267a8d47e26c9c0bf8a39342aad500ba8b
Carrie Walsh <carriewalsh@gmail.com>
2019-02-26 09:28:53 -0700

    Adds tests for add_ingredient

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -14,4 +14,16 @@ class RecipeTest < MiniTest::Test
   def test_recipe_has_name
     assert_equal "Mac and Cheese", @mac_and_cheese.name
   end
+
+  def test_recipe_starts_with_no_ingredients_by_default
+    assert_equal ({}), @mac_and_cheese.ingredients_required
+  end
+
+  def test_recipe_can_have_ingredients_added
+    @mac_and_cheese.add_ingredient(@cheese,2)
+    @mac_and_cheese.add_ingredient(@mac,8)
+    expected = {@cheese => 2,
+                @mac => 8}
+    assert_equal expected, @mac_and_cheese.ingredients_required
+  end
 end
