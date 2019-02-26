f84b3fa93ca048402455b9901df32a5ab4165315
Carrie Walsh <carriewalsh@gmail.com>
2019-02-26 09:46:19 -0700

    Adds exist test for Pantry

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,4 +1,12 @@
 require "./test/test_helper"
 
 class PantryTest < MiniTest::Test
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_pantry_exists
+    assert_instance_of Pantry, @pantry
+  end
+
 end
