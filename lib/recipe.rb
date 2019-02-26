1148e1ccb14aa1c790de796482a0be778661897e
Carrie Walsh <carriewalsh@gmail.com>
2019-02-26 09:31:09 -0700

    Adds add_ingredient method and ingredients_required to init

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,13 @@
 class Recipe
-  attr_reader :name
-  
+  attr_reader :name,
+              :ingredients_required
+
   def initialize(name)
     @name = name
+    @ingredients_required = {}
+  end
+
+  def add_ingredient(ingredient,how_many)
+    @ingredients_required[ingredient] = how_many
   end
 end
