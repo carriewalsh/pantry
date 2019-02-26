7217547cacd807b863d7a1bd1c09f6ca33436b3e
Carrie Walsh <carriewalsh@gmail.com>
2019-02-26 09:54:21 -0700

    Adds method for restock, defaults stock value to 0

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,10 +1,14 @@
 class Pantry
   attr_reader :stock
   def initialize
-    @stock = {}
+    @stock = Hash.new {|hash,key| hash[key] = 0}
   end
 
   def check_stock(ingredient)
-    @stock[ingredient] == nil ? 0 : @stock[ingredient]
+    @stock[ingredient]
+  end
+
+  def restock(ingredient,amount)
+    @stock[ingredient] += amount
   end
 end
