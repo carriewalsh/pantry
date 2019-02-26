6898361e04c619686ae73cdbc0d987472e4ec617
Carrie Walsh <carriewalsh@gmail.com>
2019-02-26 09:32:31 -0700

    Adds test for amount_required

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -26,4 +26,11 @@ class RecipeTest < MiniTest::Test
                 @mac => 8}
     assert_equal expected, @mac_and_cheese.ingredients_required
   end
+
+  def test_recipe_can_find_amount_required_for_ingredient
+    @mac_and_cheese.add_ingredient(@cheese,2)
+    @mac_and_cheese.add_ingredient(@mac,8)
+    assert_equal 2, @mac_and_cheese.amount_required(@cheese)
+    assert_equal 8, @mac_and_cheese.amount_required(@mac)
+  end
 end
